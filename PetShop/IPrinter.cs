b5af3ac4d2e0171fc6150c09451eb69b8299a29c
fabiomoniz <fabio_moniz_1@hotmail.com>
2018-08-29 11:16:09 +0100

    save

--- PetShop/IPrinter.cs
+++ PetShop/IPrinter.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace PetShop
 {
-
+    
     interface IPrinter
     {
         void StartUI();
--- PetShop/Printer.cs
+++ PetShop/Printer.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using PetShop.core.Service;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PetShop
 {
-    class Printer : IPrinter
+
+    public class Printer : IPrinter
     {
+        readonly IPetService _petService;
+        Printer(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+
         public void StartUI()
         {
             
